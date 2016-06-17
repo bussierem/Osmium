18cacc6532bac771539df9e6f70730454897c188
bussierem <max.bussiere@gmail.com>
2016-06-17 00:01:53 -0700

    tkinter annoys me enough to try PyQt5.  Got a decent start, adding more later.

--- gui/main_app.py
+++ gui/main_app.py
@@ -1,32 +1,69 @@
 import os
 from tkinter import *
 
+from PIL import Image
+
+# TODO:  ADD LINK TO https://icons8.com/ IN THE "ABOUT" PAGE!
+#   Distributed through Creative Commons Attribution-NoDerivs 3.0 Unported License
+#   https://creativecommons.org/licenses/by-nd/3.0/
+
+# Colors
+LIGHT_TEAL = "#00e6e6"
+DARK_TEAL = "#009999"
+BACKGROUND = "#6F807F"
+
+
+def resize_img(path):
+    img = Image.open(path)
+    pal = img.getpalette()
+    width, height = img.size
+    actual_transp = img.info['actual_transparency']
+    result = Image.new('LA', img.size)
+    im = img.load()
+    res = result.load()
+    for x in range(width):
+        for y in range(height):
+            t = actual_transp[im[x, y]]
+            color = pal[im[x, y]]
+            res[x, y] = (color, t)
+    result.resize((32, 32), Image.ANTIALIAS)
+    return result
+
 
 class MainApp(Frame):
     def __init__(self, master=None):
         self.master = master
-        self.frame = Frame.__init__(self, master)
-        self.pack()
+        Frame.__init__(self, master, background=BACKGROUND)
+        self.frame = Frame(self, background=BACKGROUND)
         self.current_dir = StringVar()
         self.current_search = StringVar()
         self.createWidgets()
+        self.frame.pack(side=TOP, expand=1, fill=BOTH)
+        self.pack()
 
     def createWidgets(self):
-        self.nav_frame = Frame(self.frame, bg="blue", padx=5)
-        self.nav_frame.pack(side=TOP, fill=X, expand=1, )
-        self.back_button = Button(self.nav_frame, text="<=")
-        self.back_button.pack(side=LEFT, padx=3)
-        self.forward_button = Button(self.nav_frame, text="=>")
-        self.forward_button.pack(side=LEFT, padx=3)
+        # self.nav_frame = Frame(self, padx=5, background="white")
+        self.buttons()
         self.master.update()
-        self.nav_bar = Entry(self.nav_frame, textvariable=self.current_dir)
+        self.nav_bar = Entry(
+            self.frame, textvariable=self.current_dir, font=("Source Code Pro", "12"), relief=FLAT)
         self.current_dir.set(os.getcwd())
-        self.nav_bar.pack(side=LEFT, fill=X, expand=1, padx=3)
-        self.search_bar = Entry(self.nav_frame, textvariable=self.current_search, foreground="grey")
+        self.nav_bar.grid(row=0, column=2)
+        self.search_bar = Entry(self.frame, textvariable=self.current_search, foreground="grey")
         self.current_search.set("Search")
-        self.search_bar.pack(side=LEFT, fill=X, ipadx=50, padx=3)
-        self.bottom_frame = Frame(self.frame, bg="red", padx=5)
-        self.bottom_frame.pack(side=TOP, fill=BOTH, expand=1)
+        self.search_bar.grid(row=0, column=3)
+        # self.nav_frame.pack(side=TOP, fill=X, pady=5)
+        self.bottom_frame = Frame(self.frame, padx=5, bg=BACKGROUND)
+        self.bottom_frame.grid(row=1, column=0, columnspan=4)
 
-    def say_hi(self):
-        print("hi there, everyone!")
+    def buttons(self):
+        # Back Button
+        # Image
+        back_button_black = PhotoImage(file="icons/black_icons/Left-52.png")
+        self.back_button = Button(self.frame, compound=TOP, image=back_button_black, relief=FLAT, bg=BACKGROUND)
+        self.back_button.grid(row=0, column=0)
+        self.back_button.image = back_button_black
+        forward_button_black = PhotoImage(file="icons/black_icons/Right-52.png")
+        self.forward_button = Button(self.frame, compound=TOP, image=forward_button_black, relief=FLAT, bg=BACKGROUND)
+        self.forward_button.grid(row=0, column=1)
+        self.forward_button.image = forward_button_black
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from tkinter import *
-
-from gui.main_app import MainApp
-
 if __name__ == "__main__":
-    root = Tk()
-    root.geometry('{}x{}'.format(1024, 768))
-    root.update()
-    app = MainApp(master=root)
-    app.mainloop()
+    import sys
+
+    app = QtGui.QApplication(sys.argv)
+    Dialog = QtGui.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+    # root = Tk()
+    # root.geometry('{}x{}'.format(1024, 768))
+    # root.update()
+    # app = MainApp(master=root)
+    # app.mainloop()

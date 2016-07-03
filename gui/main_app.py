ca6212b53e91dea3916b3957c9b1f670db3da992
bussierem <max.bussiere@gmail.com>
2016-07-03 10:03:32 -0700

    small amount of cleanup and additions.  time to try something...

--- gui/main_app.py
+++ gui/main_app.py
@@ -47,26 +47,71 @@ class MainApp(Frame):
         self.master.update()
         self.toolbar = Frame(self.master, bd=1, relief=RAISED)
         self.create_buttons()
-        self.nav_bar = Entry(self.toolbar, textvariable=self.current_dir, font=("Source Code Pro", "12"), relief=FLAT)
+        self.nav_bar = Entry(
+            self.toolbar,
+            textvariable=self.current_dir,
+            font=("Source Code Pro", "12"),
+            relief=FLAT
+        )
         self.current_dir.set(os.getcwd())
         self.nav_bar.pack(side=LEFT, padx=2, fill=X, expand=True)
-        self.search_bar = Entry(self.toolbar, textvariable=self.current_search, font=("Source Code Pro", "12"),
-                                foreground="grey")
-        self.current_search.set("Search")
+        self.search_bar = Entry(
+            self.toolbar,
+            textvariable=self.current_search,
+            font=("Source Code Pro", "12"),
+            foreground="grey"
+        )
+        self.current_search.set("Search {}".format(os.path.expanduser('~')))
+        self.search_bar.bind('<Button-1>', self.clear_search_bar)
+        self.search_bar.bind('<Return>', self.handle_enter)
         self.search_bar.pack(side=RIGHT)
         self.toolbar.pack(side=TOP, fill=X)
+
+    def clear_search_bar(self, event):
+        self.search_bar.delete(0, 'end')
+
+    def handle_enter(self, event):
+        f_widget = event.widget
+        if f_widget == self.search_bar:
+            # TODO:  Call searching here
+            print("TODO:  Call search method")
+        elif f_widget == self.nav_bar:
+            # TODO:  Call "go to location" here
+            print("TODO:  Call 'goto location' method")
+
+    def create_buttons(self):
+        self.back_button = Button(
+            self.toolbar,
+            relief=FLAT,
+            font=("Source Code Pro", "12"),
+            text="<="
+        )
+        self.back_button.pack(side=LEFT)
+        self.forward_button = Button(
+            self.toolbar,
+            relief=FLAT,
+            font=("Source Code Pro", "12"),
+            text="=>"
+        )
+        self.forward_button.pack(side=LEFT)
 
     def render_main_frame(self):
         self.main_frame = Frame(self.master)
         self.render_tree_sidebar()
+        self.render_file_grid()
         self.main_frame.pack(side=TOP, fill=BOTH, expand=True)
 
     def render_tree_sidebar(self):
         self.tree_frame = Frame(self.main_frame)
         self.tree_sidebar = ttk.Treeview(self.tree_frame, show='tree')
         self.tree_sidebar.bind("<<TreeviewOpen>>", self.treeview_item_opened)
-        self.tree_y_scroll = ttk.Scrollbar(self.tree_frame, orient='vertical', command=self.tree_sidebar.yview)
-        self.tree_x_scroll = ttk.Scrollbar(self.tree_frame, orient='horizontal', command=self.tree_sidebar.xview)
+        self.tree_sidebar.bind("<Button-1>", self.treeview_item_clicked)
+        self.tree_y_scroll = ttk.Scrollbar(
+            self.tree_frame, orient='vertical', command=self.tree_sidebar.yview
+        )
+        self.tree_x_scroll = ttk.Scrollbar(
+            self.tree_frame, orient='horizontal', command=self.tree_sidebar.xview
+        )
         self.tree_sidebar.configure(yscroll=self.tree_y_scroll.set, xscroll=self.tree_x_scroll.set)
         self.fill_treeview()
         self.tree_sidebar.grid(row=0, column=0, sticky=NSEW)
@@ -82,6 +127,10 @@ class MainApp(Frame):
         for d in [sub for sub in os.listdir(parent) if os.path.isdir(os.path.join(parent, sub))]:
             self.get_subdirs(os.path.join(parent, d))
 
+    def treeview_item_clicked(self, event):
+        loc = self.tree_sidebar.identify('item', event.x, event.y)
+        # TODO:  Call "goto location" method
+
     def get_subdirs(self, parent_dir):
         try:
             subdirs = [name for name in os.listdir(parent_dir)
@@ -92,7 +141,9 @@ class MainApp(Frame):
             self.tree_sidebar.insert(parent_dir, 'end', os.path.join(parent_dir, sub), text=sub)
 
     def fill_treeview(self):
-        # TODO: Bookmarks  (Desktop, Documents, Downloads)
+        for loc in ['Desktop', 'Documents', 'Downloads']:
+            path = os.path.join(os.path.expanduser('~'), loc)
+            self.tree_sidebar.insert('', 'end', path, text=loc)
         drives = get_used_drive_letters(self.OS_TYPE)
         for key in drives.keys():
             if key == drives[key]:
@@ -101,8 +152,5 @@ class MainApp(Frame):
                 self.tree_sidebar.insert('', 'end', key, text="{} ({})".format(drives[key], key))
             self.get_subdirs(key)
 
-    def create_buttons(self):
-        self.back_button = Button(self.toolbar, relief=FLAT, font=("Source Code Pro", "12"), text="<=")
-        self.back_button.pack(side=LEFT)
-        self.forward_button = Button(self.toolbar, relief=FLAT, font=("Source Code Pro", "12"), text="=>")
-        self.forward_button.pack(side=LEFT)
+    def render_file_grid(self):
+        pass

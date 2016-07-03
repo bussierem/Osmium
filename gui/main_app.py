f80a6a5331f95d39f8189ab8361df6c7971e3b14
bussierem <max.bussiere@gmail.com>
2016-07-02 21:29:20 -0700

    Made some interesting progress.   Went back to Tkinter

--- gui/main_app.py
+++ gui/main_app.py
@@ -1,5 +1,8 @@
 import os
+import win32api
+from collections import OrderedDict
 from tkinter import *
+from tkinter import ttk
 
 from PIL import Image
 
@@ -30,40 +33,109 @@ def resize_img(path):
     return result
 
 
+def get_used_drive_letters(os):
+    drive_lbls = OrderedDict()
+    if os == "Windows":
+        #                           WINDOWS
+        # ---------------------------------
+        drives = win32api.GetLogicalDriveStrings()
+        drives = drives.split('\000')[:-1]
+        for d in drives:
+            drive_lbls[d] = d
+            try:
+                info = win32api.GetVolumeInformation(d)
+                drive_lbls[d] = info[0] if info[0] != '' else d
+            except:
+                continue
+    elif os == "Linux":
+        #                             LINUX
+        # ---------------------------------
+        # Linux does not list drive letters
+        drive_lbls = {'/': 'Root', '/home/': 'Home'}
+    return drive_lbls
+
+
 class MainApp(Frame):
     def __init__(self, master=None):
         self.master = master
         Frame.__init__(self, master, background=BACKGROUND)
-        self.frame = Frame(self, background=BACKGROUND)
+        self.get_os_type()
         self.current_dir = StringVar()
         self.current_search = StringVar()
-        self.createWidgets()
-        self.frame.pack(side=TOP, expand=1, fill=BOTH)
+        self.render_toolbar()
+        self.render_main_frame()
         self.pack()
 
-    def createWidgets(self):
-        # self.nav_frame = Frame(self, padx=5, background="white")
-        self.buttons()
-        self.master.update()
-        self.nav_bar = Entry(
-            self.frame, textvariable=self.current_dir, font=("Source Code Pro", "12"), relief=FLAT)
-        self.current_dir.set(os.getcwd())
-        self.nav_bar.grid(row=0, column=2)
-        self.search_bar = Entry(self.frame, textvariable=self.current_search, foreground="grey")
-        self.current_search.set("Search")
-        self.search_bar.grid(row=0, column=3)
-        # self.nav_frame.pack(side=TOP, fill=X, pady=5)
-        self.bottom_frame = Frame(self.frame, padx=5, bg=BACKGROUND)
-        self.bottom_frame.grid(row=1, column=0, columnspan=4)
+    def get_os_type(self):
+        if sys.platform.startswith('darwin'):
+            self.OS_TYPE = "Mac"
+        elif os.name == "nt":
+            self.OS_TYPE = "Windows"
+        else:
+            self.OS_TYPE = "Unix"
 
-    def buttons(self):
-        # Back Button
-        # Image
-        back_button_black = PhotoImage(file="icons/black_icons/Left-52.png")
-        self.back_button = Button(self.frame, compound=TOP, image=back_button_black, relief=FLAT, bg=BACKGROUND)
-        self.back_button.grid(row=0, column=0)
-        self.back_button.image = back_button_black
-        forward_button_black = PhotoImage(file="icons/black_icons/Right-52.png")
-        self.forward_button = Button(self.frame, compound=TOP, image=forward_button_black, relief=FLAT, bg=BACKGROUND)
-        self.forward_button.grid(row=0, column=1)
-        self.forward_button.image = forward_button_black
+    def render_toolbar(self):
+        self.master.update()
+        self.toolbar = Frame(self.master, bd=1, relief=RAISED)
+        self.create_buttons()
+        self.nav_bar = Entry(self.toolbar, textvariable=self.current_dir, font=("Source Code Pro", "12"), relief=FLAT)
+        self.current_dir.set(os.getcwd())
+        self.nav_bar.pack(side=LEFT, padx=2, fill=X, expand=True)
+        self.search_bar = Entry(self.toolbar, textvariable=self.current_search, font=("Source Code Pro", "12"),
+                                foreground="grey")
+        self.current_search.set("Search")
+        self.search_bar.pack(side=RIGHT)
+        self.toolbar.pack(side=TOP, fill=X)
+
+    def render_main_frame(self):
+        self.main_frame = Frame(self.master)
+        self.tree_sidebar = ttk.Treeview(self.main_frame, show='tree')
+        self.fill_treeview()
+        self.tree_sidebar.pack(side=LEFT, pady=2, fill=BOTH, expand=True)
+        self.main_frame.pack(side=TOP, fill=BOTH, expand=True)
+
+    def get_subdirs(self, parent_dir):
+        try:
+            subdirs = [name for name in os.listdir(parent_dir)
+                       if os.path.isdir(os.path.join(parent_dir, name))]
+        except:
+            return
+        for sub in subdirs:
+            self.tree_sidebar.insert(parent_dir, 'end', os.path.join(parent_dir, sub), text=sub)
+
+    def fill_treeview(self):
+        """
+        Things to display top-level:
+            - Bookmarks
+                - Desktop
+                - Documents
+                - Downloads
+            - Show all active mounted drives
+            - LINUX:
+                /home/
+                /
+            - WINDOWS:
+                All drive letters [Name (Letter), or just Letter]
+        """
+        drives = get_used_drive_letters(self.OS_TYPE)
+        for key in drives.keys():
+            if key == drives[key]:
+                self.tree_sidebar.insert('', 'end', key, text=drives[key])
+            else:
+                self.tree_sidebar.insert('', 'end', key, text="{} ({})".format(drives[key], key))
+            self.get_subdirs(key)
+
+    def create_buttons(self):
+        #                              BACK
+        # ---------------------------------
+        # back_button_black = PhotoImage(file="icons/black_icons/Left-52.png")
+        self.back_button = Button(self.toolbar, relief=FLAT, text="<-")
+        self.back_button.pack(side=LEFT)
+        # self.back_button.image = back_button_black
+        #                           FORWARD
+        # ---------------------------------
+        # forward_button_black = PhotoImage(file="icons/black_icons/Right-52.png")
+        self.forward_button = Button(self.toolbar, relief=FLAT, text="->")
+        self.forward_button.pack(side=LEFT)
+        # self.forward_button.grid(row=0, column=1)
+        # self.forward_button.image = forward_button_black
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-if __name__ == "__main__":
-    import sys
+from tkinter import Tk
 
-    app = QtGui.QApplication(sys.argv)
-    Dialog = QtGui.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
-    sys.exit(app.exec_())
-    # root = Tk()
-    # root.geometry('{}x{}'.format(1024, 768))
-    # root.update()
-    # app = MainApp(master=root)
-    # app.mainloop()
+from gui.main_app import MainApp
+
+if __name__ == "__main__":
+    # app = QtGui.QApplication(sys.argv)
+    # Dialog = QtGui.QDialog()
+    # ui = Ui_Dialog()
+    # ui.setupUi(Dialog)
+    # Dialog.show()
+    # sys.exit(app.exec_())
+    root = Tk()
+    root.geometry('{}x{}'.format(1024, 768))
+    root.update()
+    app = MainApp(master=root)
+    app.mainloop()
